659d2a72df93fede1cbbbd651c563daa91e730cd
Zakaria KM <zakariya@innoveins.com>
2014-06-01 20:15:46 +0530

    Agent form html integration

--- application/views/admin/agents.php
+++ application/views/admin/agents.php
@@ -1,9 +1,10 @@
 <div>
+
 </div>
 <ul class="list-panel">
     <?php if ($agents) {
         foreach($agents as $agent) { ?>
-            <li id="agent-1" class="item media agent-item">
+            <li id="agent-<?=$agent['Agent']['id']; ?>" class="item media agent-item">
                 <div class="media-body pull-left">
                     <section class="title"><?=$agent['Agent']['company']; ?></section>
                     <section class="contact">
@@ -51,8 +52,8 @@
                         <?php } else { ?>
                             <?php $btnType = ($agent['Agent']['status'] == 'approved') ? 'btn-success' : 'btn-warning'; ?>
                             <button class="toggle-status btn btn-mini <?=$btnType; ?>" type="button" data-type="agent" data-action="change_status" id="<?=$agent['Agent']['id']; ?>" data-value="<?=$agent['Agent']['status']; ?>" title="Click to Change Status"><?=ucwords($agent['Agent']['status']); ?></button>
-                            <button class="btn btn-mini" type="button" data-type="agent" data-action="add-funds" id="<?=$agent['Agent']['id']; ?>" title="Add Funds"><i class="icon-plus-sign"></i> Funds</button>
-                            <a href="<?=SITE_URL; ?>/admin/agent_edit/<?=$agent['Agent']['id']; ?>" id="<?=$agent['Agent']['id']; ?>" title="Edit Agent Details <?=$agent['Agent']['company']; ?>" class="btn btn-mini"><i class="icon-pencil"></i></a>
+                            <a href="#agentFundModal" data-toggle="modal" class="btn btn-mini" data-type="agent" data-action="add-funds" title="Add Funds"><i class="icon-plus-sign"></i> Funds</a>
+                            <a href="#agentEditModal" data-toggle="modal" id="<?=$agent['Agent']['id']; ?>" title="Edit Agent Details <?=$agent['Agent']['company']; ?>" class="btn btn-mini"><i class="icon-pencil"></i></a>
                             <a href="javascript:void(0);" id="<?=$agent['Agent']['id']; ?>" data-name="<?=$agent['Agent']['company']; ?>" class="agent-delete btn btn-mini" title="Delete Agent <?=$agent['Agent']['company']; ?>"><i class="icon-trash"></i></a>
                         <?php } ?>
                     </span>
@@ -60,3 +61,84 @@
             </li>
         <?php } } ?>
 </ul>
+<!--Begin:Modal for agent fund-->
+        <div id="agentFundModal" class="modal hide fade " tabindex="-1" role="dialog" aria-labelledby="agentFundModalLabel" aria-hidden="true">
+                <div class="modal-header well">
+                  <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                  <h3 style="color: 90c53f;" id="agentFundModalLabel">Agent Funds</h3>
+                </div>
+                <div class="modal-body well">
+                    <form id="AgentAddFund" class="form-horizontal agent-form" method="post">
+                        <input type="hidden" name="agent_id" value="<?=$agent['Agent']['id']; ?>">
+                    <h2><?=$agent['Agent']['company']; ?></h2>
+
+                    <div class="control-group">
+                        <label class="control-label">Add Fund</label>
+                        <div class="controls"><input name="addFund" id="" type="text" class="span4" required=""></div>
+                    </div>
+                    <div class="control-group">
+                        <label class="control-label"></label>
+                        <div class="controls">
+                            <input type="submit" class="btn btn-success" value="Add">
+                        </div>
+                    </div>
+                    </form>
+                </div>
+              </div>
+        <!--End:Modal for agent fund-->
+        <!--Begin:Agent Edit Modal-->
+        <div id="agentEditModal" class="modal hide fade " tabindex="-1" role="dialog" aria-labelledby="agentEditModalLabel" aria-hidden="true">
+                <div class="modal-header well">
+                  <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                  <h3 style="color: 90c53f;" id="agentEditModalLabel">Edit Agent Info : <?=$agent['Agent']['company']; ?></h3>
+                </div>
+                <div class="modal-body well">
+                    <form id="AgentEdit<?=$agent['Agent']['id']; ?>" class="form-horizontal agent-form" method="post" >
+                    <input type="hidden" name="mm_form" value="registerAgent">
+                    
+                    <div class="control-group">
+                        <label class="control-label">Name of Company</label>
+                        <div class="controls"><input name="agent[company]" id="agent_company" type="text" class="span4" required=""></div>
+                    </div>
+                    <div class="control-group">
+                        <label class="control-label">Email Address</label>
+                        <div class="controls"><input name="agent[email]" id="agent_email" type="text" class="span4" required=""></div>
+                    </div>
+                    <div class="control-group">
+                        <label class="control-label">Contact Person</label>
+                        <div class="controls"><input name="agent[contact]" id="agent_contact" type="text" class="span4" required=""></div>
+                    </div>
+                    <div class="control-group">
+                        <label class="control-label">City</label>
+                        <div class="controls"><input name="agent[city]" id="agent_city" type="text" class="span4" required=""></div>
+                    </div>
+                    <div class="control-group">
+                        <label class="control-label">Country</label>
+                        <div class="controls"><input name="agent[country]" id="agent_country" type="text" class="span4" required=""></div>
+                    </div>
+                    <div class="control-group">
+                        <label class="control-label">Phone Number</label>
+                        <div class="controls"><input name="agent[phone]" id="agent_phone" type="text" class="span4" required=""></div>
+                    </div>
+                    <div class="control-group">
+                        <label class="control-label"></label>
+                        <div class="controls">
+                            <input type="submit" class="btn btn-success" value="Save">
+                        </div>
+                    </div>
+                </form>
+                </div>
+              </div>
+        <!--End:Agent Edit Modal-->
+        <script>
+            $(function(){
+
+                $('#AgentAddFund').submit(function(){
+                    alert("form submited");
+
+                    return false;
+                });
+
+
+            })
+        </script>
