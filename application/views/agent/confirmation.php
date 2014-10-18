e0a6d5a97fbd9b002ebc695a20ab3edcd8b0a0d1
Zakariya <zakariya@innoveins.com>
2014-10-18 12:30:41 +0530

    committing the changes

--- application/views/agent/confirmation.php
+++ application/views/agent/confirmation.php
@@ -1,11 +1,11 @@
 <!-- Start: Main Content --->
 <div class="content bg-white" xmlns="http://www.w3.org/1999/html">
     <div class="container">
-        <div class="heading"><h1>Agent: Confirmation</h1></div>
+        <div class="heading"><h1>Agent: Form Submission</h1></div>
         <div class="container-main box-shadow pull-left">
             <h1 class="thanks">Thank You!</h1>
-            <p>Welcome to <span class="green-title">GreenOasis</span>.</p>
-            <p>We thank you for the interest shown for being an agent of GreenOasis. The details submitted by you are as under;</p>
+            <p>Welcome to <span class="green-title">Green Oasis</span>.</p>
+            <p>We thank you for the interest showed to become an agent of Green Oasis. The details submitted by you are as under : </p>
             <ul class="piped-info pull-left clearfix">
                 <li class="pull-left bigbox">
                     <span class="pull-left info-section">
@@ -24,9 +24,15 @@
             </ul>
             <p>Our representative will get in touch with you shortly to discuss this engagement with us.</p>
             <div class="footer">
-            <h1>Terms Conditions:</h1>
-                <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.</p>
-                <p>Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.</p>
+            <h1>Contact Us : </h1>
+                
+				<p><strong>GREEN OASIS TOURISM LLC</strong><br/>
+				<p>#205 Sheikh Rashis Building<br/>
+				<p>Al Ittihad Road, Hor Al Anz East Diera, Dubai.<br/>
+				<p>Office: (+971) 4298-3811 Fax: (+971) 4268-3822<br/><br/>
+				<p>Mr. Mehmet<br/>
+				<p>E-mail : gm@dubaigot.com<br/>
+				<p>Mobile :  +971 50 4617410</p>
             </div>
         </div>
         <div class="sidebar pull-right">
